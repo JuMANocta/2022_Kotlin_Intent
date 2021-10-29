d4a0bc6cc933843a518da4c73fba446cda17b15b
JuMANocta <julien.mangeat@reseau-cd.net>
2021-10-29 10:22:59 +0200

    exemple Intent

--- app/src/main/java/com/example/testintent/MainActivity.kt
+++ app/src/main/java/com/example/testintent/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.testintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val but = findViewById<Button>(R.id.button);
+        val intent = Intent(this,MainActivity2::class.java)
+        but.setOnClickListener {
+            Toast.makeText(this, "essai", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+        }
     }
 }
